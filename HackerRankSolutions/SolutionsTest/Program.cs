890889253f45f069ecf63e206f3b1bc5c2b2ccb4
Şahin Gülci <sahingulci@gmail.com>
2017-04-26 11:24:03 +0300

    First Commit

---
+++ HackerRankSolutions/SolutionsTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using WarmupChallenges;
+
+namespace SolutionsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SolveMeFirst.Solve();
+            SimpleArraySum.Solve();
+
+        }
+    }
+}
---
+++ HackerRankSolutions/WarmupChallenges/SimpleArraySum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace WarmupChallenges
+{
+    public class SimpleArraySum
+    {
+        public static void Solve()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+
+
+            Console.WriteLine(arr.Sum()); 
+        }
+    }
+}
