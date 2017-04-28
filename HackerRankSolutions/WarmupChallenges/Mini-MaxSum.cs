50a461e778ce5a696fd20986e87b5cda62bd578a
Şahin Gülci <sahingulci@gmail.com>
2017-04-28 15:51:13 +0300

    more solution

---
+++ HackerRankSolutions/WarmupChallenges/Mini-MaxSum.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Linq;
+
+namespace WarmupChallenges
+{
+    public class Mini_MaxSum
+    {
+        public static void Solve()
+        {
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            long[] arr = Array.ConvertAll(arr_temp, long.Parse);
+
+            solution(arr);
+        }
+
+
+        static void solution(long[] arr)
+        {
+            long[] sums = new long[5];
+            long sumOfOneLine;
+
+            for (int i = 0; i < 5; i++)
+            {
+                sumOfOneLine = 0;
+
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i != j )
+                    {
+                        sumOfOneLine += arr[j];
+                    }
+                }
+
+                sums[i] = sumOfOneLine;
+            }
+
+            Console.WriteLine(sums.Min() + " " + sums.Max()); ;
+        }
+    }
+}
---
+++ HackerRankSolutions/WarmupChallenges/TimeConversion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WarmupChallenges
+{
+    public class TimeConversion
+    {
+        public static void Solve()
+        {
+            string initialTime = Console.ReadLine();
+
+            solution(initialTime);
+        }
+
+
+        static void solution(string time)
+        {
+
+
+        }
+    }
+}
