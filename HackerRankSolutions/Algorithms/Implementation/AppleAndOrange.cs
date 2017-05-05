ca7b28809fa8c769ad5540fda1da6a96df73fa15
Şahin Gülci <sahingulci@gmail.com>
2017-05-05 16:38:17 +0300

    more solution

---
+++ HackerRankSolutions/Algorithms/Implementation/AppleAndOrange.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Implementation
+{
+    public class AppleAndOrange
+    {
+        public static void Solve()
+        {
+            string[] tokens_s = Console.ReadLine().Split(' ');
+            int s = Convert.ToInt32(tokens_s[0]);
+            int t = Convert.ToInt32(tokens_s[1]);
+            string[] tokens_a = Console.ReadLine().Split(' ');
+            int a = Convert.ToInt32(tokens_a[0]);
+            int b = Convert.ToInt32(tokens_a[1]);
+            string[] tokens_m = Console.ReadLine().Split(' ');
+            int m = Convert.ToInt32(tokens_m[0]);
+            int n = Convert.ToInt32(tokens_m[1]);
+            string[] apple_temp = Console.ReadLine().Split(' ');
+            int[] apple = Array.ConvertAll(apple_temp, Int32.Parse);
+            string[] orange_temp = Console.ReadLine().Split(' ');
+            int[] orange = Array.ConvertAll(orange_temp, Int32.Parse);
+        }
+
+        static void solution()
+        {
+
+
+
+        }
+    }
+}
---
+++ HackerRankSolutions/Algorithms/Implementation/GradingStudents.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Implementation
+{
+    public class GradingStudents
+    {
+        public void Solve()
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] grades = new int[n];
+
+            for (int grades_i = 0; grades_i < n; grades_i++)
+            {
+                grades[grades_i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int[] result = solve(grades);
+
+            Console.WriteLine(String.Join("\n", result));
+
+        }
+
+        static int[] solve(int[] grades)
+        {
+            List<int> res = new List<int>();
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                int rem = 0;
+
+                Math.DivRem(grades[i], 5, out rem);
+
+                if (grades[i] < 38 || rem < 3)
+                {
+                    res.Add(grades[i]);
+                }
+                else
+                {
+                    res.Add(grades[i] + (5 - rem));
+                }
+            }
+
+            return res.ToArray();
+        }
+    }
+}
