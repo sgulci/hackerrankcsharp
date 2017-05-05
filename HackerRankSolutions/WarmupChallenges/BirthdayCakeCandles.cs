5ce409b80f8b5cc290dc98fb5c5f3dd8b7bbf25d
Şahin Gülci <sahingulci@gmail.com>
2017-05-05 14:18:15 +0300

    warmups finished

---
+++ HackerRankSolutions/WarmupChallenges/BirthdayCakeCandles.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace WarmupChallenges
+{
+    public class BirthdayCakeCandles
+    {
+        public static void Solve()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] height_temp = Console.ReadLine().Split(' ');
+            int[] height = Array.ConvertAll(height_temp, Int32.Parse);
+
+            solution(n,height);
+        }
+
+        static void solution(int n, int[] heights)
+        {
+            int tallest = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (tallest < heights[i])
+                {
+                    tallest = heights[i];
+                }
+            }
+
+            int tallestCount = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (heights[i] == tallest)
+                {
+                    tallestCount++;
+                }
+            }
+            Console.WriteLine(tallestCount);
+        }
+    }
+}
--- HackerRankSolutions/WarmupChallenges/TimeConversion.cs
+++ HackerRankSolutions/WarmupChallenges/TimeConversion.cs
@@ -14,7 +14,23 @@ namespace WarmupChallenges
 
         static void solution(string time)
         {
+            string res = "";
 
+            string[] timeArr = time.Substring(0, 8).Split(':');
+            string checkPM = time.Substring(time.Length - 2);
+
+            if (checkPM == "PM" && timeArr[0] != "12")
+            {
+                timeArr[0] = (Convert.ToInt16(timeArr[0]) + 12).ToString();
+            }
+            else if (checkPM == "AM" && timeArr[0] == "12")
+            {
+                timeArr[0] = "00";
+            }
+
+            res = string.Join(":", timeArr);
+
+            Console.WriteLine(res);
 
         }
     }
